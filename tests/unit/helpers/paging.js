0f1e37ebc06913bdb9f83f6f8e8fcc5284060f13
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-03-11 01:41:32 -0300

    Paging helper tests

---
+++ tests/unit/helpers/paging.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const assert = require('assert').strict;
+
+const {
+	helpers: {
+		Paging
+	}
+} = require('../../../lib');
+
+describe('Helpers', () => {
+	describe('Paging', () => {
+
+		describe('parseRequestPagingParams()', () => {
+
+			context('When an invalid page number or size is received', () => {
+
+				it('Should throw if page number is not a number', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams('invalid-number'));
+				});
+
+				it('Should throw if page number is not an integer', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams(1.5));
+				});
+
+				it('Should throw if page number is lower than one', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams(0));
+					assert.throws(() => paging.parseRequestPagingParams(-10));
+				});
+
+				it('Should throw if page size is not a number', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams(null, 'invalid-number'));
+				});
+
+				it('Should throw if page size is not an integer', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams(null, 1.5));
+				});
+
+				it('Should throw if page size is lower than one', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams(null, 0));
+					assert.throws(() => paging.parseRequestPagingParams(null, -10));
+				});
+
+				it('Should throw if page size is higher than max size', () => {
+					const paging = new Paging();
+					assert.throws(() => paging.parseRequestPagingParams(null, 200));
+				});
+
+				it('Should throw if page size is higher than provided size', () => {
+					const paging = new Paging(null, 2);
+					assert.throws(() => paging.parseRequestPagingParams(null, 5));
+				});
+			});
+
+			context('When default values are used', () => {
+				it('Should return default page number and size', () => {
+					const paging = new Paging();
+					const result = paging.parseRequestPagingParams();
+
+					assert.deepStrictEqual(result, {
+						pageNumber: 1,
+						pageSize: 10
+					});
+				});
+				it('Should return the provided default page number and size', () => {
+					const paging = new Paging(30);
+					const result = paging.parseRequestPagingParams();
+
+					assert.deepStrictEqual(result, {
+						pageNumber: 1,
+						pageSize: 30
+					});
+				});
+			});
+
+			context('When valid sort field and criteria are received', () => {
+				it('Should return the specified page number and size', () => {
+					const paging = new Paging();
+					const result = paging.parseRequestPagingParams(2, 50);
+
+					assert.deepStrictEqual(result, {
+						pageNumber: 2,
+						pageSize: 50
+					});
+				});
+			});
+		});
+
+	});
+});
